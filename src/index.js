5049cf5292b806552e14c2d4ed41aa28363ccf8c
Joseph Hassell <josephh2018@gmail.com>
2018-05-02 22:29:30 -0500

    use strict and licence at top

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+'use strict';
+
 /*
 MIT License
 
-Copyright (c) 2017 Joseph Hassell
+Copyright (c) 2017-2018 Joseph Hassell
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
